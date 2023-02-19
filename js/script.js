39ae5ef0075d65b2e87685d19f3cb2da01859d1a
muhamedkrbalo <mohamedkrbalo@gmail.com>
2023-02-19 10:30:29 +0200

    agency profile

--- js/script.js
+++ js/script.js
@@ -682,6 +682,7 @@ $(function(){
     responsiveClass: true,
     margin: 20,
     rtl: true,
+    navText:[<i class="fa-regular fa-chevron-left"></i>,<i class="fa-regular fa-chevron-right"></i>],
     responsive: {
         0: {
             items: 3
